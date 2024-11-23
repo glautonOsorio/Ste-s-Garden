e83bfa787b5ebb74d089a8babee1db7de1c4c44e
Glauton Osório <glautonleao@gmail.com>
2024-11-22 23:51:32 -0300

    feat: atulizando coisas a pedido da minha namorada que eu amo muito

--- scripts/ajuda.js
+++ scripts/ajuda.js
@@ -1,13 +1,17 @@
-$(document).ready(function() {
-    var maxHeight = 0;
-    
-    $('.accordion-item').each(function() {
-      var itemHeight = $(this).outerHeight();
-      if (itemHeight > maxHeight) {
-        maxHeight = itemHeight;
-      }
-    });
-  
-    $('.accordion-item').height(maxHeight);
+document.querySelectorAll(".accordion-header").forEach((header) => {
+  header.addEventListener("click", () => {
+    const item = header.parentElement;
+    const content = item.querySelector(".accordion-content");
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null; // Fecha o item atual
+    } else {
+      // Fecha todos os outros itens
+      document.querySelectorAll(".accordion-content").forEach((content) => {
+        content.style.maxHeight = null;
+      });
+
+      content.style.maxHeight = content.scrollHeight + "px"; // Abre o item atual
+    }
   });
-  
+});
